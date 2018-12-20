8b1d9b379e12271a9bd9ba8ceb8793f29a4b710f
Omar Maynard <o.maynard@reply.eu>
2018-12-20 17:12:03 +0000

    added session token and login auth

--- Safe Hands V1/models/user.js
+++ Safe Hands V1/models/user.js
@@ -17,6 +17,26 @@ var UserSchema = new mongoose.Schema({
         require : true
     }
 });
+// authenticate input against data base documents
+UserSchema.statics.authenticate = function(email, password, callback){
+    User.findOne({email: email})
+    .exec(function (error, user) {
+        if (error) {
+            return callback(error);
+        } else if ( !user ) {
+            var err = new Error('User not found.');
+            err.status = 401;
+            return callback(err);
+        }
+        bcrypt.compare(password, user.password, function(error, result){
+            if (result === true) {
+                return callback(null, user);
+            } else {
+                return callback();
+            }
+        })
+    });
+}
 // hash password before saving to datebase
 UserSchema.pre('save', function(next) {
     var user = this;
